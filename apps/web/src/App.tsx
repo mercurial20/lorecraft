0a0079ffcd21875f998467983ff2ea9e0f4b8282
Medet Kozhabayev <kozhabayev20@gmail.com>
2026-05-28 18:54:18 +0500

    feat(web): add shadcn and it's theme support

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,62 @@
+import { useTheme } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
+
 function App() {
-  return <>test</>;
+  const { theme, setTheme } = useTheme();
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+        <CardAction>
+          <Button variant="link">Sign Up</Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2"></div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+        <Button
+          onClick={() => {
+            setTheme(theme === "light" ? "dark" : "light");
+          }}
+        >
+          Change Theme to {theme === "light" ? "dark" : "light"}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default App;
